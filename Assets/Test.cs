1b46c251df70cdad623c9daee82aebc60c55f85b
KyogoTerashima <kyogoterashima@outlook.jp>
2020-04-18 20:20:42 +0900

    Lesson4課題提出　
    新規プロジェクト作成
    Testスクリプト作成

---
+++ Assets/Test.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Boss
+{
+    private int mp = 53;
+    
+    public void Magic()
+    {
+        this.mp -= 5;
+        int magic = this.mp;
+
+        if(mp >= 0)
+        {
+
+        Debug.Log("魔法攻撃をした。残りMPは" + magic);
+        }
+        else
+        {
+            Debug.Log("MPが足りないため魔法が使えない。");
+        }
+    }
+} 
+
+public class Test : MonoBehaviour {
+
+    // Use this for initialization
+
+    void Start()
+    {
+        int[] array = {20, 30, 40, 80, 90};
+
+        for(int i = 0; i < array.Length; i++)
+        {
+            Debug.Log(array[i]);
+        }
+        for(int t = 5-1; t >= 0; t--)
+        {
+            Debug.Log(array[t]);
+        }
+        Boss magic = new Boss();
+
+        magic.Magic();
+
+        for(int t = 0; t <= 10; t++)
+        {
+            magic.Magic();
+        }
+        magic.Magic();
+    }
+
+}
+
+
+
